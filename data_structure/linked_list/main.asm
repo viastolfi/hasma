6e754c26cf9b80a00bf3bc73cb68d1c7f2f63d3d
viastolfi <vincent.astolfi12@gmail.com>
2026-04-28 21:28:36 +0200

    feat: base for ll_push

--- data_structure/linked_list/main.asm
+++ data_structure/linked_list/main.asm
@@ -1,5 +1,8 @@
 %include "ll.inc"
 
+section .data
+  val0 dq 10
+
 section .bss
   ll resb LL_size
 
@@ -14,10 +17,22 @@ _start:
   cmp rax, -1
   je .panic
 
-  cmp qword [ll + LL.head], 0
+  cmp qword [rel ll + LL.head], 0
   jne .panic
 
-  cmp qword [ll + LL.esize], 8
+  cmp qword [rel ll + LL.esize], 8
+  jne .panic
+
+  mov rdi, ll
+  mov rsi, val0
+  call _ll_push
+
+  cmp qword [rel ll + LL.head], 0
+  je .panic
+
+  mov r12, qword [rel ll + LL.head]
+  mov r12, qword [r12 + NODE.ptr]
+  cmp qword [r12], 10
   jne .panic
   
   mov rax, 60
